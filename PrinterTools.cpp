a70c6c07ac2906f35c910a3ea1f89b24ca897635
sherpya <sherpya@netfarm.it>
2009-07-29 16:02:16 +0200

    added crude error messages in RawPrint

--- PrinterTools.cpp
+++ PrinterTools.cpp
@@ -2,7 +2,15 @@
 #include "resource.h"
 #include "Dialogs.h"
 
-#define CALL(func) do { if (!func) { err = GetLastError(); goto end; } } while (0)
+#define CALL(func, msg) do \
+{ \
+    if (!func) \
+    { \
+        err = GetLastError(); \
+        ::MessageBox(NULL, _T(#msg), _T("Error"), MB_OK | MB_ICONERROR); \
+        goto end; \
+    } \
+} while (0)
 
 BOOL CMainDlg::EnumerateLocalPrinters(HTREEITEM parent)
 {
@@ -145,19 +153,22 @@ BOOL CMainDlg::RawPrint(LPTSTR fileName)
         GENERIC_READ, FILE_SHARE_READ,
         NULL, OPEN_EXISTING,
             FILE_ATTRIBUTE_NORMAL, NULL)) == INVALID_HANDLE_VALUE)
+    {
+        ::MessageBox(NULL, _T("CreateFile"), fileName, MB_OK);
             goto end;
+    }
 
     size = ::GetFileSize(f, NULL);
     buffer = (char *) new char[size];
     ::ReadFile(f, buffer, size, &r, NULL);
     ::CloseHandle(f);
 
-    CALL(::OpenPrinter(printer.GetBuffer(0), &p, &defaults));
-    CALL(::StartDocPrinter(p, 1, (LPBYTE) &doc));
-    CALL(::StartPagePrinter(p));
-    CALL(::WritePrinter(p, buffer, size, &w));
-    CALL(::EndPagePrinter(p));
-    CALL(::EndDocPrinter(p));
+    CALL(::OpenPrinter(printer.GetBuffer(0), &p, &defaults), OpenPrinter);
+    CALL(::StartDocPrinter(p, 1, (LPBYTE) &doc), StartDocPrinter);
+    CALL(::StartPagePrinter(p), StartPagePrinter);
+    CALL(::WritePrinter(p, buffer, size, &w), WritePrinter);
+    CALL(::EndPagePrinter(p), EndPagePrinter);
+    CALL(::EndDocPrinter(p), EndDocPrinter);
 
 end:
     if (buffer) delete buffer;
