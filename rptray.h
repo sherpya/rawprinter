363bdcee812074ce383a0a40e8e4b1d8290c2e1c
sherpya <sherpya@netfarm.it>
2009-08-03 17:56:18 +0200

    check windows version for ballon message

--- rptray.h
+++ rptray.h
@@ -15,13 +15,26 @@ public:
         memset(&m_nid, 0, sizeof(m_nid));
         m_nid.cbSize        = sizeof(NOTIFYICONDATA);
         m_nid.uVersion      = NOTIFYICON_VERSION;
-	    m_nid.hIcon         = (HICON) ::LoadImage(_Module.GetResourceInstance(), MAKEINTRESOURCE(IDI_ICON), IMAGE_ICON, ::GetSystemMetrics(SM_CXICON), ::GetSystemMetrics(SM_CYICON), LR_DEFAULTCOLOR);
+	    m_nid.hIcon         = (HICON) ::LoadImage(_Module.GetResourceInstance(),
+            MAKEINTRESOURCE(IDI_ICON_PRINTER), IMAGE_ICON,
+            ::GetSystemMetrics(SM_CXICON), ::GetSystemMetrics(SM_CYICON), LR_DEFAULTCOLOR);
         m_nid.hWnd          = m_hWnd;
         m_nid.uID           = 0;
 
         _tcscpy(m_nid.szInfoTitle, _T("RawPrinter"));
-
         AddIcon();
+
+        OSVERSIONINFO osv;
+        memset(&osv, 0, sizeof(osv));
+        osv.dwOSVersionInfoSize = sizeof(OSVERSIONINFO);
+
+        m_nid.dwInfoFlags = NIIF_NONE;
+        if (!::GetVersionEx(&osv))
+            return;
+
+        if (osv.dwMajorVersion >= 5)
+            m_nid.dwInfoFlags = NIIF_INFO | NIIF_USER;
+        
     }
     void AddIcon(void)
     {
@@ -30,8 +43,10 @@ public:
     }
     void Ballon(WTL::CString &message, UINT timeout)
     {
+        if (m_nid.dwInfoFlags == NIIF_NONE)
+            return;
+
         m_nid.uFlags        = NIF_INFO;
-        m_nid.dwInfoFlags   = NIF_MESSAGE | NIF_ICON | NIF_TIP | NIIF_USER;
         m_nid.uTimeout      = timeout;
         _tcsncpy(m_nid.szInfo, message.GetBuffer(0), (sizeof(m_nid.szInfo) / sizeof(TCHAR)) - 1);
         ::Shell_NotifyIcon(NIM_MODIFY, &m_nid);
